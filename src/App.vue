4bd2deba615606593a538622fcadd8fd94c84f66
Ricardoporfiriovieira <ricardoporfiriovieira@gmail.com>
2023-08-26 01:59:40 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- Create new ref by setting "ref" prop -->
+    <input
+      type="text"
+      placeholder="Text"
+      ref="inputField"
+    />
+    <button @click="sendMessage">Shout</button>
+    <br />
+    <b>{{ shout }}</b>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      shout: '',
+    };
+  },
+  methods: {
+    sendMessage() {
+      const message = this.$refs.inputField.value;
+      axios
+        .post("http://localhost:8000/message/")
+      this.shout = `${message.toUpperCase()}!!!`;
+    },
+  },
+};
+</script>
